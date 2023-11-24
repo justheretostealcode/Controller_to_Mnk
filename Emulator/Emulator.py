465c57ec2cbb4652b28c9e1d6f86b3440bb3ea2a
Zwackelmann <alexanderdiallo78@gmail.com>
2023-11-24 22:18:19 +0100

    Defined basic Emulator functionality

---
+++ Emulator/Emulator.py
@@ -0,0 +1,18 @@
+
+
+class Emulator:
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def load_controller_input(self):
+        pass
+
+    def emulate_mouse(self):
+        pass
+
+    def emulate_keyboard(self):
+        pass

a3e87cfb05f7fdcee85b722e255d3e34571ed24f
Zwackelmann <alexanderdiallo78@gmail.com>
2023-11-24 21:58:54 +0100

    Defined basic SettingsParser functionality

---
+++ Settings/SettingsParser.py
@@ -0,0 +1,13 @@
+class SettingsParser:
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def load_mouse_settings(self):
+        pass
+
+    def load_keyboard_settings(self):
+        pass
